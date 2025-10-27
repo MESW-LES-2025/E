a3ad9686558db8eba3a9eec941ae87f4ca2867e3
AFONSO PIMENTA <afonsocpimenta04@gmail.com>
2025-10-27 02:16:00 +0000

    chore(frontend): add api fetch demo
    Signed-off-by: AFONSO PIMENTA <afonsocpimenta04@gmail.com>

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import TestComponent from "../components/TestComponent";
 import { Calendar } from "@/components/ui/calendar";
@@ -11,18 +14,49 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
+type Event = {
+  name: string;
+  date: string;
+};
+
 export default function Home() {
+  const [events, setEvents] = useState<Event[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/events/`
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch events");
+        }
+        const data: { results: Event[] } = await response.json();
+        setEvents(data.results);
+      } catch (err: unknown) {
+        setError((err as Error).message || "Unknown error");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  console.log("console.log events:", events);
+  console.log("events length:", events.length);
   return (
     <div>
       <h1>Hello, World!</h1>
       <p>Welcome to the home page!</p>
+      <br />
+      API fetch example:
+      <br />
       <TestComponent />
       <br />
-      We could use shadcn <br />
-      Those are premade components, already styled and allow customization.
-      <br />
-      Here are some examples <br />
-      link: https://ui.shadcn.com/docs/components
+      Shadcn UI examples:
       <br />
       <Button>Click Me</Button>
       <Calendar
@@ -45,6 +79,23 @@ export default function Home() {
           </SelectGroup>
         </SelectContent>
       </Select>
+      <br />
+      <h2>Events:</h2>
+      {loading ? (
+        <p>Loading events...</p>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : events.length > 0 ? (
+        <ul>
+          {events.map((event) => (
+            <li key={event.name}>
+              {event.name} — {new Date(event.date).toLocaleString()}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No events available</p>
+      )}
     </div>
   );
 }
