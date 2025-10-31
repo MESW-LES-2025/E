28dad011ac758c2fa68264f1e05bbe81c539e3fa
AFONSO PIMENTA <afonsocpimenta04@gmail.com>
2025-10-31 01:17:40 +0000

    chore(frontend): setup deployment to github pages
    Signed-off-by: AFONSO PIMENTA <afonsocpimenta04@gmail.com>

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const env = process.env.NODE_ENV;
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: { unoptimized: true },
+  basePath:
+    env === "development" ? "" : env === "production" ? "/prod" : "/staging",
+  assetPrefix:
+    env === "development" ? "" : env === "production" ? "/prod/" : "/staging/",
 };
 
 export default nextConfig;
