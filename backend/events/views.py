feda596a2cf78cff0bf9c54c1b4730d92d31d406
AFONSO PIMENTA <afonsocpimenta04@gmail.com>
2025-11-03 01:03:00 +0000

    test: testing stage deployment
    Signed-off-by: AFONSO PIMENTA <afonsocpimenta04@gmail.com>

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.response import Response
 
 from .models import Event
 from .serializers import EventSerializer
@@ -7,6 +8,14 @@ from .serializers import EventSerializer
 class EventListCreateView(generics.ListCreateAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        response.data = {
+            "static_string": "This is a static string",
+            "events": response.data,
+        }
+        return Response(response.data)
 
 
 class EventRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
