4e5cc9d8005639dde71fab8bc7d91c2017d222b0
AFONSO PIMENTA <afonsocpimenta04@gmail.com>
2025-11-06 03:16:21 +0000

    chore: setup JWT authentication on django settings
    Signed-off-by: AFONSO PIMENTA <afonsocpimenta04@gmail.com>

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,14 +1,11 @@
-from django.utils import timezone  # type: ignore
-from rest_framework import generics
+from django.utils import timezone
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import status
-
-import logging
 
 from .models import Event
 from .serializers import EventSerializer
 
-logger = logging.getLogger(__name__)
 
 class EventListCreateView(generics.ListCreateAPIView):
     queryset = Event.objects.all()
@@ -27,17 +24,16 @@ class UpcomingEventsListView(generics.ListAPIView):
         now = timezone.now()
         return Event.objects.filter(date__gte=now).order_by("date")
 
+
 class CreateEventView(generics.CreateAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
-        logger.info(f"CreateEventView called with data: {request.data}")
         serializer = self.get_serializer(data=request.data)
-        
+
         if serializer.is_valid():
             self.perform_create(serializer)
-            logger.info(f"Event created successfully: {serializer.data}")
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        logger.error(f"Event creation failed with errors: {serializer.errors}")
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
