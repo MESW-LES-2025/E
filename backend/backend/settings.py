00cb8202c6dcedc300a0fd5e5aae37db2433d594
pveloso <pedrovelosofernandes@outlook.com>
2025-11-09 18:31:00 +0000

    Installed djoser, a REST implementation of Django authentication system.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "corsheaders",
     "events",
+    "djoser",
 ]
 
 MIDDLEWARE = [
@@ -136,6 +137,10 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+}
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ("JWT",),
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
